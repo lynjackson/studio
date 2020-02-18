ca23d74c451575eb766933678ea97fb91ba40e6f
bklyn <lynjacksonoconnor@gmail.com>
2020-02-18 10:06:46 -0500

    changed structure to a simpler, er, non-next build

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = ()=>{
+    return(
+        <div>blah</div>
+    )
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: "./src/app.js",
+  mode: "development",
+  
+  output: {
+    filename: "[name].bundle.js",
+    chunkFilename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/",
+  },
+
+  devServer: {
+    contentBase: './dist',
+    port: 3000,
+    // publicPath: "http://localhost:3000/dist/",
+    // hotOnly: true
+  },
+  
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  
+  
+  plugins: [
+    // new webpack.HotModuleReplacementPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'output',
+      template: './index.html'
+    })
+  ]
+};
